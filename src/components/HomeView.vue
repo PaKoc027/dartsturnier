71264a837d690a690f551b5e9015d7aae39e1e1c
alberteiste1n <git@alberteistein.dev>
2025-12-17 03:40:27 +0100

    Add frontend router

---
+++ src/components/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <ul>
+    <li>
+      <button>
+        <h2>🎯 Maltes Tunier</h2>
+      </button>
+    </li>
+  </ul>
+
+  <RouterLink to="create">Tunier Erstellen</RouterLink>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+import HomeView from '@/components/HomeView.vue';
+import CreateView from "@/components/CreateView.vue";
+
+const routes = [
+    { path: '/', component: HomeView },
+    { path: '/create', component: CreateView },
+]
+
+export const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+})
+
+const app = createApp(App)
+app.use(router).mount('#app')
+
